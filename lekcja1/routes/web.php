16eecad4363965b7e50b3415aaf370252c912036
creeper82 <jan.szuszkiewicz06@gmail.com>
2023-09-20 08:39:48 +0200

    Add Status route

--- lekcja1/routes/web.php
+++ lekcja1/routes/web.php
@@ -26,6 +26,10 @@ Route::get("/helloworld", function() { return view('helloworld');});
 
 Route::redirect("/", "/helloworld");
 
+Route::get('/status', function() {
+    return "Status";
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');

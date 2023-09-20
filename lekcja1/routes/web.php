c6d0548c6f7dcc757c0d406fc26b4c379dbd5506
creeper82 <jan.szuszkiewicz06@gmail.com>
2023-09-20 08:34:08 +0200

    Add a sample redirect route

--- lekcja1/routes/web.php
+++ lekcja1/routes/web.php
@@ -24,6 +24,8 @@ Route::get('/dashboard', function () {
 
 Route::get("/helloworld", function() { return view('helloworld');});
 
+Route::redirect("/", "/helloworld");
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');

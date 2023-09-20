48b2169eabd7a3ae78f0ba9d28a57dbf6b9a4a31
creeper82 <jan.szuszkiewicz06@gmail.com>
2023-09-20 09:25:35 +0200

    Update hello world params display

--- lekcja1/resources/views/helloworld.blade.php
+++ lekcja1/resources/views/helloworld.blade.php
@@ -51,7 +51,7 @@
                                 <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">This text is edited lol</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                        Welcome to hello world page..
+                                        Welcome to the hello world page.. <br>
                                         Your full name seems to be: {{$name}} {{$surname}}
                                 </p>
                             </div>

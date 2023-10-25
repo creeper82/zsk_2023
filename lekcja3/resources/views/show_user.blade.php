f871930c26d2b1de4d2dc27bc9d3183efa60e7dc
creeper82 <jan.szuszkiewicz06@gmail.com>
2023-10-25 09:05:54 +0300

    Fix some space

--- lekcja3/resources/views/show_user.blade.php
+++ lekcja3/resources/views/show_user.blade.php
@@ -1,6 +1,6 @@
 <div>
     <h3>Dane użytkownika</h3>
-    <p>Imie masz { {$firstName }}</p>
+    <p>Imie masz {{$firstName }}</p>
     <p>Nazwisko {{ $lastName }}</p>
     <p>Miasto {{ $city }}</p>
     <p>Lisrta twoich hobbbbbbbyyyyyy</p>

b34b3669b2d15fbfa071f592a9e453b274cccb53
creeper82 <jan.szuszkiewicz06@gmail.com>
2023-11-08 09:24:11 +0100

    Add errors under inputs

--- lekcja3/resources/views/forms/user_form.blade.php
+++ lekcja3/resources/views/forms/user_form.blade.php
@@ -19,10 +19,18 @@
     @endif
 
     <form action="UserFormController" method="GET">
-        <input type="text" name="firstName" placeholder="Podaj imię" autofocus> <br> <br>
-        <input type="text" name="lastName" placeholder="Podaj nazwisko"> <br> <br>
-        <input type="text" name="city" placeholder="Podaj miasto"> <br> <br>
-        <input type="text" name="hobbies" placeholder="Podaj hobby po przecinku"> <br> <br>
+        <input type="text" name="firstName" placeholder="Podaj imię" autofocus> <br>
+        @error("firstName") <p>{{$message}}</p> @enderror
+        
+        <input type="text" name="lastName" placeholder="Podaj nazwisko"> <br>
+        @error("lastName") <p>{{$message}}</p> @enderror
+        
+        <input type="text" name="city" placeholder="Podaj miasto"> <br> 
+        @error("city") <p>{{$message}}</p> @enderror
+        
+        <input type="text" name="hobbies" placeholder="Podaj hobby po przecinku"> <br>
+        @error("hobbies") <p>{{$message}}</p> @enderror
+        
         <input type="submit" value="Zatwierdź dane">
     </form>
 </div>

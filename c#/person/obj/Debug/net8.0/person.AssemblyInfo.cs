4ff776b8049f1a29f31e02de2918ca6aca659afd
creeper82 <jan.szuszkiewicz06@gmail.com>
2024-01-17 08:02:14 +0100

    Add new project and clean up files

--- c#/person/obj/Debug/net8.0/person.AssemblyInfo.cs
+++ c#/person/obj/Debug/net8.0/person.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("person")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5803a2bd62435c1d69cf43a89369d26a0f7ba90e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c5c7f59a194af1036d659ce4c665d48f18de282")]
 [assembly: System.Reflection.AssemblyProductAttribute("person")]
 [assembly: System.Reflection.AssemblyTitleAttribute("person")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

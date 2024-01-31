da67fb9a4ef7d162868672ec8fb6a49174d64a5d
creeper82 <jan.szuszkiewicz06@gmail.com>
2024-01-31 08:31:55 +0100

    Add new project

---
+++ c#/interfejsy_book/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace interfejsy_book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

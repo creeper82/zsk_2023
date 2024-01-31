9793213ff3ebe783f08759c50f08c561215d8b3b
creeper82 <jan.szuszkiewicz06@gmail.com>
2024-01-31 08:45:08 +0100

    Add book class with CompareTo method

--- c#/interfejsy_book/Program.cs
+++ c#/interfejsy_book/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using classes;
 
 namespace interfejsy_book
 {
---
+++ c#/interfejsy_book/classes/Book.cs
@@ -0,0 +1,21 @@
+namespace classes;
+
+public class Book : Icomparable<Book>
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public ushort Year { get; set; }
+    public float Price { get; set; }
+
+    public Book(string title, string author, ushort year, float price) {
+        Title = title;
+        Author = author;
+        Year = year;
+        Price = price;
+    }
+
+    public int CompareTo(Book other) {
+        if (other is null) return 1;
+        return Price.CompareTo(other.Price);
+    }
+}

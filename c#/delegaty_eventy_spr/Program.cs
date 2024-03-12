1a5a037cee0b2791a254c16fcc231f2afc8962c2
Janek <66107554+creeper82@users.noreply.github.com>
2024-03-12 18:01:13 +0100

    Add a sample usage of classes

--- c#/delegaty_eventy_spr/Program.cs
+++ c#/delegaty_eventy_spr/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using Classes;
+
+Food f1 = new("Jabłko", 300);
+Dog dog = new("pudel", "Czips", "idk species", 12);
+Animal dogAsAnimal = new Dog("pudel", "Czips(animal)", "idk species", 12);
+
+f1.PrintInfo();
+dog.PrintInfo();
+dog.MakeSound();
+dogAsAnimal.PrintInfo();
+dogAsAnimal.MakeSound();

5b8817332ce1bdce1fb11d10b08426430368cddb
Janek <66107554+creeper82@users.noreply.github.com>
2024-03-12 18:03:42 +0100

    Update the Animal class to include event

--- c#/delegaty_eventy_spr/classes/Animal.cs
+++ c#/delegaty_eventy_spr/classes/Animal.cs
@@ -2,28 +2,29 @@ namespace Classes;
 
 public class Animal
 {
+    public delegate void AnimalSoundHandler();
+
     public string Name { get; set; }
     public string Species { get; set; }
     public byte Age { get; set; }
+    public event AnimalSoundHandler? AnimalSoundEvent;
 
     public Animal(string name, string species, byte age)
     {
         Name = name; Species = species; Age = age;
     }
 
-    public virtual void MakeSound() { }
+    public void MakeSound() { AnimalSoundEvent?.Invoke(); }
 
     public void PrintInfo() { Console.WriteLine($"Zwierzę gatunku {Species} ma nazwę {Name} i wiek {Age} lat/a."); }
 }
 
-public class Dog : Animal {
-    public string Breed {get; set;}
-    
-    public Dog(string breed, string name, string species, byte age) : base(name, species, age) {
-        Breed = breed;
-    }
+public class Dog : Animal
+{
+    public string Breed { get; set; }
 
-    public override void MakeSound() {
-        Console.WriteLine($"Woof! From dog {Name}");
+    public Dog(string breed, string name, string species, byte age) : base(name, species, age)
+    {
+        Breed = breed;
     }
 }

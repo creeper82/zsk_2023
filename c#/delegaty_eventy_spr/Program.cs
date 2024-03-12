d55d1edc9b0e7aeb48a902b6b11188030b451c23
Janek <66107554+creeper82@users.noreply.github.com>
2024-03-12 18:07:23 +0100

    Update the Program.cs with event subscription

--- c#/delegaty_eventy_spr/Program.cs
+++ c#/delegaty_eventy_spr/Program.cs
@@ -4,6 +4,12 @@ Food f1 = new("Jabłko", 300);
 Dog dog = new("pudel", "Czips", "idk species", 12);
 Animal dogAsAnimal = new Dog("pudel", "Czips(animal)", "idk species", 12);
 
+dog.AnimalSoundEvent +=
+    n => { Console.WriteLine($"dog named {n} made sound."); };
+
+dogAsAnimal.AnimalSoundEvent +=
+    n => { Console.WriteLine($"dog casted to animal, named {n} made sound."); };
+
 f1.PrintInfo();
 dog.PrintInfo();
 dog.MakeSound();
